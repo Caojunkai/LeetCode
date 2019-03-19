c9fec60adfef9455c13c8f6881924a957e7046c4
fitzix <caojunkaiv@gmail.com>
2019-03-19 19:55:45 +0800

    feat: add 6-zigzag-conversion
    add 6-zigzag-conversion solution

---
+++ leetcode-cn/6.z-字形变换.go
@@ -0,0 +1,83 @@
+import "bytes"
+
+/*
+ * @lc app=leetcode.cn id=6 lang=golang
+ *
+ * [6] Z 字形变换
+ *
+ * https://leetcode-cn.com/problems/zigzag-conversion/description/
+ *
+ * algorithms
+ * Medium (40.95%)
+ * Total Accepted:    23.6K
+ * Total Submissions: 57.7K
+ * Testcase Example:  '"PAYPALISHIRING"\n3'
+ *
+ * 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+ *
+ * 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+ *
+ * L   C   I   R
+ * E T O E S I I G
+ * E   D   H   N
+ *
+ *
+ * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+ *
+ * 请你实现这个将字符串进行指定行数变换的函数：
+ *
+ * string convert(string s, int numRows);
+ *
+ * 示例 1:
+ *
+ * 输入: s = "LEETCODEISHIRING", numRows = 3
+ * 输出: "LCIRETOESIIGEDHN"
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: s = "LEETCODEISHIRING", numRows = 4
+ * 输出: "LDREOEIIECIHNTSG"
+ * 解释:
+ *
+ * L     D     R
+ * E   O E   I I
+ * E C   I H   N
+ * T     S     G
+ *
+ */
+func convert(s string, numRows int) string {
+	if numRows == 1 || len(s) <= numRows {
+		return s
+	}
+
+	res := bytes.Buffer{}
+	// p pace 步距
+	p := numRows*2 - 2
+
+	// 处理第一行
+	for i := 0; i < len(s); i += p {
+		res.WriteByte(s[i])
+	}
+
+	// 处理中间的行
+	for r := 1; r <= numRows-2; r++ {
+		// 添加r行的第一个字符
+		res.WriteByte(s[r])
+
+		for k := p; k-r < len(s); k += p {
+			res.WriteByte(s[k-r])
+			if k+r < len(s) {
+				res.WriteByte(s[k+r])
+			}
+		}
+	}
+
+	// 处理最后一行
+	for i := numRows - 1; i < len(s); i += p {
+		res.WriteByte(s[i])
+	}
+
+	return res.String()
+}
+
