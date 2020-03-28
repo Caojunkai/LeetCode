a54151c19b02c79e30847159bf4e32a575588201
fitzix <caojunkaiv@gmail.com>
2020-03-28 11:46:16 +0800

    feat: x

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/fitzix/leetcode
 
-go 1.12
+go 1.14
---
+++ leetcodecn/914.卡牌分组.go
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn id=914 lang=golang
+ *
+ * [914] 卡牌分组
+ */
+package leetcodecn
+
+// @lc code=start
+func hasGroupsSizeX(deck []int) bool {
+}
+
+// @lc code=end
